156d2c5f7d260bf8cf6b62cd786218c9b7e1560f
Alexandro Silva <alexandro@ikons.com.br>
2020-12-30 00:46:57 -0300

    test: ensure SignUpController returns 200 if AddAccount returns a valid account

--- src/presentation/controllers/signup/signup.spec.ts
+++ src/presentation/controllers/signup/signup.spec.ts
@@ -206,4 +206,24 @@ describe('SignUp Controller', () => {
     expect(httpResponse.statusCode).toBe(500)
     expect(httpResponse.body).toEqual(new ServerError())
   })
+
+  test('Should return 200 if AddAccount returns a valid account', () => {
+    const { sut } = makeSut()
+    const httpRequest: HttpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_mail@acme.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(200)
+    expect(httpResponse.body).toEqual({
+      id: 'valid_id',
+      name: 'valid_name',
+      email: 'valid_mail@acme.com',
+      password: 'valid_password'
+    })
+  })
 })
