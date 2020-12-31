e31e6034e781b8c0ca37dd0c51da688fdf6c84c6
Alexandro Silva <alexandro@ikons.com.br>
2020-12-31 13:49:33 -0300

    test: ensure DbAddAccount throw if AddAccountRepository throws

--- src/domain/data/usecases/add-account/db-add-account.spec.ts
+++ src/domain/data/usecases/add-account/db-add-account.spec.ts
@@ -90,4 +90,19 @@ describe('DbAddAccount Usecase', () => {
       password: 'hashed_password'
     })
   })
+
+  test('Should throw if AddAccountRepository throws', async () => {
+    const { sut, addAccountRepositoryStub } = makeSut()
+    jest.spyOn(addAccountRepositoryStub, 'add').mockReturnValueOnce(
+      Promise.reject(new Error())
+    )
+
+    const accountData = {
+      name: 'valid_name',
+      email: 'valid_email@acme.com',
+      password: 'valid_password'
+    }
+    const promise = sut.add(accountData)
+    await expect(promise).rejects.toThrowError()
+  })
 })
