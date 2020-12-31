1143795b4423aae68c341cc08d8163f97858d29b
Alexandro Silva <alexandro@ikons.com.br>
2020-12-31 13:57:44 -0300

    test: ensure DbAddAccount return an account on success

--- src/domain/data/usecases/add-account/db-add-account.spec.ts
+++ src/domain/data/usecases/add-account/db-add-account.spec.ts
@@ -105,4 +105,21 @@ describe('DbAddAccount Usecase', () => {
     const promise = sut.add(accountData)
     await expect(promise).rejects.toThrowError()
   })
+
+  test('Should return an account on success', async () => {
+    const { sut } = makeSut()
+
+    const accountData = {
+      name: 'valid_name',
+      email: 'valid_email@acme.com',
+      password: 'valid_password'
+    }
+    const account = await sut.add(accountData)
+    expect(account).toEqual({
+      id: 'valid_id',
+      name: 'valid_name',
+      email: 'valid_email@acme.com',
+      password: 'hashed_password'
+    })
+  })
 })
