65a0ccd203192c359b82c90737125bfba17bfadc
Alexandro Silva <alexandro@ikons.com.br>
2021-01-02 20:26:55 -0300

    test: ensure LogControllerDecorator call controller handle with same params

---
+++ src/main/decorators/log.spec.ts
@@ -0,0 +1,52 @@
+import { LogControllerDecorator } from './log'
+import { HttpRequest, HttpResponse } from '../../presentation/protocols/http'
+import { Controller } from '../../presentation/protocols'
+
+interface SutType {
+  sut: LogControllerDecorator
+  controllerStub: Controller
+}
+
+const makeController = (): Controller => {
+  class ControllerStub implements Controller {
+    async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+      const fakeResponse: HttpResponse = {
+        statusCode: 200,
+        body: {
+          name: 'any_name'
+        }
+      }
+      return Promise.resolve(fakeResponse)
+    }
+  }
+
+  return new ControllerStub()
+}
+
+const makeSut = (): SutType => {
+  const controllerStub = makeController()
+  const sut = new LogControllerDecorator(controllerStub)
+
+  return {
+    sut,
+    controllerStub
+  }
+}
+
+describe('LogController Decorator', () => {
+  test('Should call controller handle', async () => {
+    const { sut, controllerStub } = makeSut()
+    const handleSpy = jest.spyOn(controllerStub, 'handle')
+    const httpRequest: HttpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_mail@acme.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+
+    await sut.handle(httpRequest)
+    expect(handleSpy).toHaveBeenCalledWith(httpRequest)
+  })
+})
