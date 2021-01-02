d2bdf89a34a55e109571bd2dac0645cb58464685
Alexandro Silva <alexandro@ikons.com.br>
2021-01-02 20:25:52 -0300

    feat: ensure LogControllerDecorator call controller handle with same params

---
+++ src/main/decorators/log.ts
@@ -0,0 +1,15 @@
+import { Controller } from '../../presentation/protocols'
+import { HttpRequest, HttpResponse } from '../../presentation/protocols/http'
+
+export class LogControllerDecorator implements Controller {
+  constructor (
+    private readonly controller: Controller
+  ) {
+
+  }
+
+  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+    const httpResponse = await this.controller.handle(httpRequest)
+    return httpResponse
+  }
+}
