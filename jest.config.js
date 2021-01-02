c401678a3ba677f36d98bcec7f8108e55f916f4d
Alexandro Silva <alexandro@ikons.com.br>
2021-01-02 00:54:49 -0300

    chore: remove protocols files from coverage

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,11 @@
 module.exports = {
   roots: ['<rootDir>/src/'],
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/**/*-protocols.ts',
+    '!<rootDir>/src/**/index.ts',
+    '!<rootDir>/src/main/**'
+  ],
   coverageDirectory: 'coverage',
   preset: '@shelf/jest-mongodb',
   transform: {
