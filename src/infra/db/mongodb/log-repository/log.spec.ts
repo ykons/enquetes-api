79ed9951b232eee6f46ae88e25fffbff3fbc9d3c
Alexandro Silva <alexandro@ikons.com.br>
2021-01-03 19:01:49 -0300

    test: ensure LogMongoRepository create an error log on success

---
+++ src/infra/db/mongodb/log-repository/log.spec.ts
@@ -0,0 +1,30 @@
+import { MongoHelper } from '../helpers/mongo-helper'
+import { LogMongoRepository } from './log'
+
+describe('Log Mongo Repository', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect()
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    const errorCollection = await MongoHelper.getCollection('errors')
+    await errorCollection.deleteMany({})
+  })
+
+  const makeSut = (): LogMongoRepository => {
+    return new LogMongoRepository()
+  }
+
+  test('Should create an error log on success', async () => {
+    const sut = makeSut()
+
+    await sut.logError('any_error')
+    const errorCollection = await MongoHelper.getCollection('errors')
+    const count = await errorCollection.countDocuments()
+    expect(count).toBe(1)
+  })
+})
