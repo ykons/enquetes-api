d3324be8679b71f1fd987085b7d56592bc68df0f
Alexandro Silva <alexandro@ikons.com.br>
2021-01-09 13:24:40 -0300

    feat: ensure DbAuthentication behavior run correctly

---
+++ src/data/usecases/authentication/db-authentication.ts
@@ -0,0 +1,23 @@
+import { Authentication, AuthenticationModel, HashComparer, LoadAccountByEmailRepository, TokenGenerator, UpdateAccessTokenRepository } from './db-authentication-protocols'
+
+export class DbAuthentication implements Authentication {
+  constructor (
+    private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository,
+    private readonly updateAccessTokenRepository: UpdateAccessTokenRepository,
+    private readonly hashComparer: HashComparer,
+    private readonly tokenGenerator: TokenGenerator
+  ) { }
+
+  async auth (authentication: AuthenticationModel): Promise<string | null> {
+    const account = await this.loadAccountByEmailRepository.load(authentication.email)
+    if (account != null) {
+      const isValid = await this.hashComparer.compare(authentication.password, account.password)
+      if (isValid) {
+        const accessToken = await this.tokenGenerator.generate(account.id)
+        await this.updateAccessTokenRepository.update(account.id, accessToken)
+        return accessToken
+      }
+    }
+    return null
+  }
+}
--- src/main/decorators/log.spec.ts
+++ src/main/decorators/log.spec.ts
@@ -2,7 +2,7 @@ import { LogControllerDecorator } from './log'
 import { HttpRequest, HttpResponse, Controller } from '../../presentation/protocols'
 import { ok, serverError } from '../../presentation/helpers/http/http-helper'
 import { AccountModel } from '../../domain/models/account'
-import { LogErrorRepository } from '../../data/protocols/log-error-repository'
+import { LogErrorRepository } from '../../data/protocols/db/log-error-repository'
 
 interface SutType {
   sut: LogControllerDecorator
