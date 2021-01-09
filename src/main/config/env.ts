b05cf6c5a94898306801b16b0fe10616c96e48f8
Alexandro Silva <alexandro@ikons.com.br>
2021-01-09 20:22:40 -0300

    chore: enable docker, nodemon and debug with vscode

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,5 @@
 export default {
-  mongoUri: process.env.MONGO_URL ?? 'mongodb://127.0.0.1:27017/enquetes',
+  mongoUri: process.env.MONGO_URL ?? 'mongodb://mongo:27017/enquetes',
   port: process.env.PORT ?? 5050,
   jwtSecret: process.env.JWT_SECRET ?? 'hZBPeyJ_FdkkLnYx4e7J'
 }
