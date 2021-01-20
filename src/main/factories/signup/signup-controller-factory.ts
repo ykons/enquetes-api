626541724d0f8424de8150622efdb4934afe87c0
Alexandro Silva <alexandro@ikons.com.br>
2021-01-19 22:28:38 -0300

    feat: ensure SignUpController calls Authentication with correct values

--- src/main/factories/signup/signup-controller-factory.ts
+++ src/main/factories/signup/signup-controller-factory.ts
@@ -6,13 +6,18 @@ import { LogMongoRepository } from '../../../infra/db/mongodb/log/log-mongo-repo
 import { Controller } from '../../../presentation/protocols'
 import { LogControllerDecorator } from '../../decorators/log-controller-decorator'
 import { makeSignUpValidation } from './signup-validation-factory'
+import { DbAuthentication } from '../../../data/usecases/authentication/db-authentication'
+import { JwtAdapter } from '../../../infra/cryptography/jwt-adapter/jwt-adapter'
+import env from '../../config/env'
 
 export const makeSignUpController = (): Controller => {
   const salt = 12
   const bcryptAdapter = new BcryptAdapter(salt)
   const accountMongoRepository = new AccountMongoRepository()
   const dbAddAccount = new DbAddAccount(bcryptAdapter, accountMongoRepository)
-  const signUpController = new SignUpController(dbAddAccount, makeSignUpValidation())
+  const jwtAdapter = new JwtAdapter(env.jwtSecret)
+  const dbAuthentication = new DbAuthentication(accountMongoRepository, accountMongoRepository, bcryptAdapter, jwtAdapter)
+  const signUpController = new SignUpController(dbAddAccount, makeSignUpValidation(), dbAuthentication)
   const logMongoRepository = new LogMongoRepository()
   return new LogControllerDecorator(signUpController, logMongoRepository)
 }
