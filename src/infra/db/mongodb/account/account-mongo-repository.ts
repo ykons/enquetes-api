6ffec142fb82e5fe5f4862b3b2d2c33ae8311097
Alexandro Silva <alexandro@ikons.com.br>
2021-01-25 15:57:49 -0300

    refactor: return null when error on insert Account to Repository

--- src/infra/db/mongodb/account/account-mongo-repository.ts
+++ src/infra/db/mongodb/account/account-mongo-repository.ts
@@ -6,9 +6,12 @@ import { AddAccountModel } from '../../../../domain/usecases/add-account'
 import { MongoHelper } from '../helpers/mongo-helper'
 
 export class AccountMongoRepository implements AddAccountRepository, LoadAccountByEmailRepository, UpdateAccessTokenRepository {
-  async add (accountData: AddAccountModel): Promise<AccountModel> {
+  async add (accountData: AddAccountModel): Promise<AccountModel | null> {
     const accountCollection = await MongoHelper.getCollection('accounts')
     const result = await accountCollection.insertOne(accountData)
+    if (result.insertedCount === 0) {
+      return null
+    }
     return MongoHelper.map(result.ops[0])
   }
 
