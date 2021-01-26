b7483c867b31e931060752cf30b862c8201983ad
Alexandro Silva <alexandro@ikons.com.br>
2021-01-26 17:21:52 -0300

    feat: ensure DbAddAccount calls LoadAccountByEmailRepository with correct email

--- src/data/usecases/add-account/db-add-account-protocols.ts
+++ src/data/usecases/add-account/db-add-account-protocols.ts
@@ -1,4 +1,5 @@
 export * from '../../protocols/cryptography/hasher'
 export * from '../../protocols/db/account/add-account-repository'
+export * from '../../protocols/db/account/load-account-by-email-repository'
 export * from '../../../domain/models/account-model'
 export * from '../../../domain/usecases/add-account'
